11278a9b03dddfc58db6ffd3598f3f1751c860bb
MATTIA ROSSO s294711 <s294711@studenti.polito.it>
2022-08-26 17:16:28 +0200

    Reviewed code

--- Code/PyCharmProject/ModelEvaluation.py
+++ Code/PyCharmProject/ModelEvaluation.py
@@ -9,20 +9,6 @@ import GMMClassifier
 
 
 class BinaryModelEvaluator:
-    """
-    @staticmethod
-    def confusion_matrix(predicted_labels, actual_labels):
-        i = 0
-        conf_matrix = np.zeros(shape=(2, 2))
-        for predicted_label in predicted_labels:
-            actual_label = actual_labels[i]
-            if predicted_label == actual_label:
-                conf_matrix[predicted_label][predicted_label] += 1
-            else:
-                conf_matrix[predicted_label][actual_label] += 1
-            i += 1
-        return conf_matrix
-    """
     @staticmethod
     def accuracy(conf_matrix):
         return (conf_matrix[0, 0] + conf_matrix[1, 1]) / (conf_matrix[0, 0] + conf_matrix[1, 1] + conf_matrix[1, 0] + conf_matrix[0, 1])
